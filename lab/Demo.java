d16304cb35e56731e00e86173fbd6df94a687a6d
Pankaj2107 <71484840+Pankaj2107@users.noreply.github.com>
2020-11-20 13:19:41 +0530

    Add files via upload

---
+++ lab/Demo.java
@@ -0,0 +1,7 @@
+import Pk1.*;
+class Demo{
+	public static void main(String args[]){
+		Pack obj = new Pack(5);
+		obj.show();
+	}
+}
---
+++ lab/Pk1/Pack.java
@@ -0,0 +1,11 @@
+package Pk1;
+public class Pack{
+	public Pack(int a){
+		this.a=a;
+	}
+	public int a;
+	public void show(){
+		System.out.println(a);
+	}
+}
+	
